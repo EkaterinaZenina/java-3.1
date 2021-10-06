c39c30d4c621b9741ed051e91c6cf76d4b4d811a
EkaterinaZenina <90308326+EkaterinaZenina@users.noreply.github.com>
2021-10-06 21:29:55 +0300

    Create BonusMilesService.java

---
+++ BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int price = 1000;
+        int accrual = 1;
+        int amount = 20;
+        int bonus = price / amount  ;
+    return bonus;
+    }
+}
